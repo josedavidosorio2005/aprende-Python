b3443aafacb2e627fae267ecec608f84b88a78cd
Jose David Osorio Gallego <filidorcrack323@gmail.com>
2025-02-13 11:54:58 -0500

    Add files via upload

---
+++ Fumdamentos Basicos/1_Variables/Variables.py
@@ -0,0 +1,40 @@
+#varibles numericos 
+
+edad = 25 #numero entero 
+Altura = 1.75 #numero decimal
+NUmero_telefono = 12.59968544752552
+
+#varibles con testo (string)
+Nombre = "maria"
+apellido = 'gonsalez'
+direccion = """calle 13 # 45-67"""
+
+#varibles booleanas
+es_estudiante = True
+tiene_mascotas = False
+
+#operacion con numeros 
+edad = 25 #numero entero
+nueva_edad = edad + 5 #suma
+
+#operacion con texto
+
+nombre_completo = Nombre +" "+ apellido
+
+#cambiar el valor de la variable 
+contador = 1
+contador = contador + 1
+
+
+#crear un programa  que calcule la edad en el 2030
+Año_actual = 2025
+año_nacimiento = 2005
+
+edad_futura = 2030 - año_nacimiento
+
+# verificacion de el tipo de variables 
+print(type(Nombre))
+print(type(edad))
+print(type(Altura))
+print(type(es_estudiante))
+print(type(NUmero_telefono))
